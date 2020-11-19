22ebb00e629eddbb7c495f18c35738a1bf90ba47
r0664n77 <70115123+r0664n77@users.noreply.github.com>
2020-11-19 22:48:17 +0300

    Update class
    Now this class fully useful

--- main.py
+++ main.py
@@ -1,54 +1,99 @@
 import numpy as np
+import sys
 
-def f(x):
-    return 2/(1 + np.exp(-x)) - 1
 
-def df(x):
-    return 0.5*(1 + x)*(1 - x)
+class CarNN(object):
 
-W1 = np.array([[-0.2, 0.3, -0.4], [0.1, -0.3, -0.4]])
-W2 = np.array([0.2, 0.3])
+    def __init__(self, learning_rate=0.1):
+        self.weights_0_1 = np.random.normal(0.0, 2 ** -0.5, (2, 4))
+        self.weights_1_2 = np.random.normal(0.0, 1, (1, 2))
+        self.sigmoid_mapper = np.vectorize(self.sigmoid)
+        self.learning_rate = np.array([learning_rate])
 
-def go_forward(inp):
-    sum = np.dot(W1, inp)
-    out = np.array([f(x) for x in sum])
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
 
-    sum = np.dot(W2, out)
-    y = f(sum)
-    return (y, out)
+    def predict(self, inputs):
+        inputs_1 = np.dot(self.weights_0_1, inputs)
+        outputs_1 = self.sigmoid_mapper(inputs_1)
 
-def train(epoch):
-    global W2, W1
-    lmd = 0.01          # шаг обучения
-    N = 10000           # число итераций при обучении
-    count = len(epoch)
-    for k in range(N):
-        x = epoch[np.random.randint(0, count)]  # случайных выбор входного сигнала из обучающей выборки
-        y, out = go_forward(x[0:3])             # прямой проход по НС и вычисление выходных значений нейронов
-        e = y - x[-1]                           # ошибка
-        delta = e*df(y)                         # локальный градиент
-        W2[0] = W2[0] - lmd * delta * out[0]    # корректировка веса первой связи
-        W2[1] = W2[1] - lmd * delta * out[1]    # корректировка веса второй связи
+        inputs_2 = np.dot(self.weights_1_2, outputs_1)
+        outputs_2 = self.sigmoid_mapper(inputs_2)
+        return outputs_2
 
-        delta2 = W2*delta*df(out)               # вектор из 2-х величин локальных градиентов
+    def train(self, inputs, expected_predict):
+        inputs_1 = np.dot(self.weights_0_1, inputs)
+        outputs_1 = self.sigmoid_mapper(inputs_1)
 
-        # корректировка связей первого слоя
-        W1[0, :] = W1[0, :] - np.array(x[0:3]) * delta2[0] * lmd
-        W1[1, :] = W1[1, :] - np.array(x[0:3]) * delta2[1] * lmd
+        inputs_2 = np.dot(self.weights_1_2, outputs_1)
+        outputs_2 = self.sigmoid_mapper(inputs_2)
 
-# обучающая выборка (она же полная выборка)
-epoch = [(-1, -1, -1, -1),
-         (-1, -1, 1, 1),
-         (-1, 1, -1, -1),
-         (-1, 1, 1, 1),
-         (1, -1, -1, -1),
-         (1, -1, 1, 1),
-         (1, 1, -1, -1),
-         (1, 1, 1, -1)]
+        actual_predict = outputs_2[0]
+        error_layer_2 = np.array([actual_predict - expected_predict])
 
-train(epoch)        # запуск обучения сети
+        gradient_layer_2 = actual_predict * (1 - actual_predict)
+        weights_delta_layer_2 = error_layer_2 * gradient_layer_2
+        self.weights_1_2 -= (np.dot(weights_delta_layer_2, outputs_1.reshape(1, len(outputs_1)))) * self.learning_rate
 
-# проверка полученных результатов
-for x in epoch:
-    y, out = go_forward(x[0:3])
-    print(f"Выходное значение НС: {y} => {x[-1]}")
+        error_layer_1 = weights_delta_layer_2 * self.weights_1_2
+        gradient_layer_1 = outputs_1 * (1 - outputs_1)
+        weights_delta_layer_1 = error_layer_1 * gradient_layer_1
+        self.weights_0_1 -= np.dot(inputs.reshape(len(inputs), 1), weights_delta_layer_1).T * self.learning_rate
+
+def MSE(y, Y):
+    return np.mean((y - Y) ** 2)
+
+
+train = [
+    # ([0, 0, 0], 0),
+    # ([0, 0, 1], 1),
+    # ([0, 1, 0], 0),
+    # ([0, 1, 1], 0),
+    # ([1, 0, 0], 1),
+    # ([1, 0, 1], 1),
+    # ([1, 1, 0], 0),
+    # ([1, 1, 1], 0),
+    ([1, 1, 1, 1], 0),
+    ([1, 1, 1, 0], 1),
+    ([1, 1, 0, 0], 0),
+    ([1, 0, 0, 0], 0),
+    ([0, 0, 0, 0], 0),
+    ([0, 1, 1, 1], 1),
+    ([0, 0, 1, 1], 1),
+    ([0, 0, 0, 1], 0),
+    ([1, 0, 1, 0], 1),
+    ([1, 1, 0, 1], 0),
+    ([1, 0, 0, 1], 0),
+    ([0, 1, 1, 0], 1),
+    ([1, 0, 0, 1], 0),
+    ([1, 0, 1, 1], 0),
+    ([0, 1, 0, 0], 0),
+    ([0, 0, 1, 0], 0),
+
+]
+epochs = 5000
+learning_rate = 0.07
+
+network = CarNN(learning_rate=learning_rate)
+
+for e in range(epochs):
+    inputs_ = []
+    correct_predictions = []
+    for input_stat, correct_predict in train:
+        network.train(np.array(input_stat), correct_predict)
+        inputs_.append(np.array(input_stat))
+        correct_predictions.append(np.array(correct_predict))
+
+    train_loss = MSE(network.predict(np.array(inputs_).T), np.array(correct_predictions))
+    sys.stdout.write("\rПрогресс: {}, Потеря: {}".format(str(100 * e / float(epochs))[:4], str(train_loss)[:5]))
+print("\n\n")
+for input_stat, correct_predict in train:
+    print("\rВходы: {} система дала результат: {}, ожидалось: {}".format(
+        str(input_stat),
+        str(network.predict(np.array(input_stat)) > .5),
+        str(correct_predict == 1)))
+# for input_stat, correct_predict in train:
+#         print("For input: {} the prediction is: {}, expected: {}".format(
+#             str(input_stat),
+#             str(network.predict(np.array(input_stat))),
+#             str(correct_predict == 1)))
